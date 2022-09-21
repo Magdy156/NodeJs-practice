6a73826c80b3e8b3679166c7d92d9e8477d3f0b7
Magdy156 <magdyhamdy156@gmail.com>
2022-09-21 17:18:11 +0200

    using body-parser to analize the data coming from a form

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const port = 2000;
-
+const port = 2200;
+let data = {};
 // require express to run server and routes
 const express = require("express");
 
 // start up an instance of app
 const app = express();
 
-// let bodyParser = require("body-parser");
-// bodyParser = bodyParser.urlencoded({ extended: true });
+let bodyParser = require("body-parser");
+bodyParser = bodyParser.urlencoded({ extended: false });
 
 //all => (get-post-put-patch-delete)
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Home</h1>");
+  res.send(
+    '<form action="/form" method="POST"><input type="text" name="firstName"><button type="submit">Send</button></form>'
+  );
 });
+app.post("/form", bodyParser, (req, res) => {
+  data = req.body;
+  res.send(data);
+});
+
 app.get("/about", (req, res) => {
   res.send("<h1>Welcome to About</h1>");
 });
